9ab2d17833b1af121d7e7d7be3d414963f08a010
clivemardon69-cyber <clive.mardon69@gmail.com>
2026-08-13 12:39:05 +0100

    Add missing gitignore

--- Start Punt Master.bat
+++ Start Punt Master.bat
@@ -1,6 +1,6 @@
 @echo off
 title Punt Master — Server
-cd /d "C:\Longshot League\longshot-league"
+cd /d "C:\Longshot League\punt-master"
 start "Punt Master Server" cmd /k npm run dev -- --host
 timeout /t 4 /nobreak >nul
 start "" http://localhost:5173/
